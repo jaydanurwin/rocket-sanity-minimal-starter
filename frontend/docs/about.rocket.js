91f8ad9d4420e598cf17414287e7aea62e31733e
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-02-27 21:00:05 -0700

    about page on the frontend

---
+++ frontend/docs/about.rocket.js
@@ -0,0 +1,43 @@
+/* START - Rocket auto generated - do not touch */
+export const sourceRelativeFilePath = 'about.rocket.js';
+import { html, baseHead, getSanityImageURL } from './recursive.data.js';
+export { html, baseHead, getSanityImageURL };
+/* END - Rocket auto generated - do not touch */
+
+import { client } from '../src/lib/sanityClient.js';
+const query = `*[_type == 'aboutPage' && _id == 'aboutPage']`;
+let response = await client
+  .fetch(query)
+  .catch((err) => console.log(err));
+let aboutPageData = response[0];
+
+const title = aboutPageData.title;
+const description = aboutPageData.description;
+const permalink = `/`;
+
+export default () => html`
+  <html lang="en">
+    <head>
+      ${baseHead({ title, description, permalink })}
+      <style>
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: Arial, Helvetica, sans-serif
+        }
+        main {
+          margin: 0 auto;
+          padding: 1rem;
+          max-width: 960px;
+        }
+      </style>
+    </head>
+    <body>
+      <main>
+        <div class="home-wrapper">
+          <h1>${aboutPageData.title}</h1>
+        </div>
+      </main>
+    </body>
+  </html>
+`;
