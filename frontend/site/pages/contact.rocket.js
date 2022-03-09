bcb1606462a7b4ec24eb73896cc852d9e1e41ffe
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-03-08 20:27:40 -0700

    started contact page and added page links

--- frontend/site/pages/contact.rocket.js
+++ frontend/site/pages/contact.rocket.js
@@ -1,18 +1,12 @@
 /* START - Rocket auto generated - do not touch */
-export const sourceRelativeFilePath = 'about.rocket.js';
+export const sourceRelativeFilePath = 'contact.rocket.js';
 import { html, baseHead, getSanityImageURL } from './recursive.data.js';
 export { html, baseHead, getSanityImageURL };
 /* END - Rocket auto generated - do not touch */
 
-import { client } from '../src/lib/sanityClient.js';
-import { sanityPortableText } from '../src/lib/sanityPortableText.js';
-const query = `*[_type == 'aboutPage' && _id == 'aboutPage']`;
-let response = await client.fetch(query).catch((err) => console.log(err));
-let aboutPageData = response[0];
-
-export const title = aboutPageData.title;
-export const description = aboutPageData.description;
-export const permalink = `/about/`;
+export const title = 'Contact Page';
+export const description = 'This is the contact page';
+export const permalink = `/contact/`;
 
 export default () => html`
   <html lang="en">
@@ -34,8 +28,7 @@ export default () => html`
     <body>
       <main>
         <div>
-          <h1>${aboutPageData.title}</h1>
-          ${sanityPortableText(aboutPageData.bodyText)}
+          <h1>Contact Page</h1>
         </div>
       </main>
     </body>
--- frontend/site/pages/index.rocket.js
+++ frontend/site/pages/index.rocket.js
@@ -18,61 +18,63 @@ export const description = 'This is the homepage';
 export const permalink = '/';
 
 export default () => html`
-<html>
-  <head>
-  ${baseHead({title, description, permalink})}
-  <style>
-h1 {
-  font-size: 3rem;
-}
-
-.home-wrapper {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  min-height: 100vh;
-}
-.hero-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-.left__section {
-  padding: 2rem;
-  display: flex;
-  flex-direction: column;
-}
-@media (max-width: 768px) {
-  .home-wrapper {
-    grid-template-columns: 1fr;
-  } 
-}
-  </style>
-  </head>
-  <body>
-  <div class="home-wrapper">
-    <section class="left__section">
-      <default-header></default-header>
-      <div class="left-content__container">
-        <h1>${homePageData.heroTitle}</h1>
-        <p>${homePageData.description}</p>
+  <html>
+    <head>
+      ${baseHead({ title, description, permalink })}
+      <style>
+        h1 {
+          font-size: 3rem;
+        }
+        .home-wrapper {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          min-height: 100vh;
+        }
+        .hero-image {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+        }
+        .left__section {
+          padding: 2rem;
+          display: flex;
+          flex-direction: column;
+        }
+        @media (max-width: 768px) {
+          .home-wrapper {
+            grid-template-columns: 1fr;
+          }
+        }
+      </style>
+    </head>
+    <body>
+      <div class="home-wrapper">
+        <section class="left__section">
+          <default-header></default-header>
+          <div class="left-content__container">
+            <h1>${homePageData.heroTitle}</h1>
+            <p>${homePageData.description}</p>
+            <a href="/about/">About Page</a>
+            <a href="/contact/">Contact Page</a>
+          </div>
+        </section>
+        <section class="right__section">
+          <picture>
+            <source
+              srcset="
+                ${getSanityImageURL(homePageData.heroImage)
+                  .format('webp')
+                  .url()}"
+              type="image/webp"
+            />
+            <img
+              class="hero-image"
+              src=${`${getSanityImageURL(homePageData.heroImage).url()}`}
+              alt="${homePageData.heroTite}"
+            />
+          </picture>
+        </section>
       </div>
-    </section>
-    <section class="right__section">
-      <picture>
-        <source
-          srcset="
-            ${getSanityImageURL(homePageData.heroImage).format('webp').url()}
-          "
-          type="image/webp"
-        />
-        <img
-          class="hero-image"
-          src=${`${getSanityImageURL(homePageData.heroImage).url()}`}
-          alt="${homePageData.heroTite}"
-        />
-      </picture>
-    </section>
-  </div>
-</body>
+    </body>
   </html>
 `;
